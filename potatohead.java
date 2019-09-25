d6f652849506e936587b0ab7ec8b337465023deb
Justin Riley <20hz20khz@gmail.com>
2019-09-25 10:38:30 -0400

    Create potatohead.java

---
+++ potatohead.java
@@ -0,0 +1,49 @@
+
+public class PotatoHead
+{
+	// create instance variables
+	private boolean hasEyes;
+	private String eyeColor;
+	// add one instance variable
+
+	// default constructor
+	public PotatoHead()
+	{
+		// initialize ***ALL*** instance variables here
+		hasEyes = false;
+		eyeColor = "unknown";
+	}
+
+	/*
+	VARIOUS ACCESSOR AND MUTATOR METHODS IN ALPHABETICAL ORDER GO HERE
+	*/
+
+	/* String getEyeColor()
+     accessor method for eyeColor
+	*/ 
+	public String getEyeColor()
+	{
+		return eyeColor;
+	}
+
+	/* void setEyeColor(String someColor)
+     MUTATOR METHOD - change an instance variable's value
+	*/ 
+	public void setEyeColor(String someColor)
+	{
+		eyeColor = someColor;
+	}
+
+	/* String toString()
+  *  ACCESSOR METHOD
+	*  method to access ***ALL*** instance variables
+	*  you should be adding your new instance variable here
+	*/ 
+	public String toString()
+	{
+		String output;
+		output =  "Eyes: " + eyes;
+		output += "\nCostume: " + costume;
+		return output;
+	}
+} 
